85a658321ad797f6e2cacfcd47756bb356515db5
Dmytro Pylypenko <dimpiax@gmail.com>
2018-10-10 19:37:15 +0200

    Initial Commit

---
+++ BerlinClock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BerlinClock
+//
+//  Created by Dmytro Pylypenko on 10/10/18.
+//  Copyright © 2018 Dmytro Pylypenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
