b2222f9322679d8bbe55f593464bee9360a8353e
Dmytro Pylypenko <dimpiax@gmail.com>
2018-10-12 01:13:12 +0200

    initial commit

---
+++ BerlinClock/models/BerlinClockModel.swift
@@ -0,0 +1,19 @@
+//
+//  BerlinClockModel.swift
+//  BerlinClock
+//
+//  Created by Dmytro Pylypenko on 10/12/18.
+//  Copyright © 2018 Dmytro Pylypenko. All rights reserved.
+//
+
+import Foundation
+
+struct BerlinClockModel {
+    static func convertBerlinTime(value: String) -> String {
+        notImplemented()
+    }
+    
+    static func convertDigitalTime(value: String, dateFormat: String = "HH:mm:ss") -> String {
+        notImplemented()
+    }
+}
---
+++ BerlinClock/utils/GloblaFunctions.swift
@@ -0,0 +1,17 @@
+//
+//  GloblaFunctions.swift
+//  BerlinClock
+//
+//  Created by Dmytro Pylypenko on 10/12/18.
+//  Copyright © 2018 Dmytro Pylypenko. All rights reserved.
+//
+
+import Foundation
+
+func notImplemented() -> Never {
+    fatalError("Not Implemented")
+}
+
+func abstractFunc() -> Never {
+    fatalError("Abstract Function")
+}
